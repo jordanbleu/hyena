6a68bd75842e18997e946e14ac8c2b74b5b18879
Jordan Bleu <jordanbleu1@gmail.com>
2023-02-12 16:40:50 -0600

    initial commit

---
+++ source/_meta/card-sounds/card-images/readme.txt
@@ -0,0 +1,3 @@
+this will eventually contain sounds that play when the game is active in the pd ui
+
+see https://sdk.play.date/1.13.0/Inside%20Playdate.html#pdxinfo 
---
+++ source/main.lua
@@ -0,0 +1,15 @@
+--[[
+
+    Project Hyena
+    by Jordan Bleu
+
+    Highly Important links:
+    ======================
+    PlayDate sdk docs: https://sdk.play.date/1.13.0/Inside%20Playdate.html
+    SquidGod video that explains his file structure: https://www.youtube.com/watch?v=PZD1Ba15nnM    
+
+]]
+
+function playdate.update()
+    print "hello world"
+end
