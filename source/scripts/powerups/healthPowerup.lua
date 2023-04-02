6e2874494f69b22cadf25581a2255ef00332824c
Jordan Bleu <jordanbleu1@gmail.com>
2023-04-02 16:00:21 -0500

    allow weapons to be unlocked.

--- source/scripts/powerups/healthPowerup.lua
+++ source/scripts/powerups/healthPowerup.lua
@@ -13,7 +13,7 @@ function HealthPowerup:_onCollected(player)
     HealthPowerup.super._onCollected(self, player)
     -- do a lil animation
     player:addHealth(15)
-    local absorb = SingleSpriteAnimation("images/powerups/healthPickupAnim/collect", 100, player.x, player.y)
+    local absorb = SingleSpriteAnimation("images/powerups/healthPickupAnim/collect", 500, player.x, player.y)
     absorb:setZIndex(50)
     local healing = SingleSpriteAnimation("images/effects/healingAnim/heal", 2000, player.x, player.y)
     healing:attachTo(player)
