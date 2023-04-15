1f86218f2a13b4dd2da7b0c648a101cc327e0756
Jordan Bleu <jordanbleu1@gmail.com>
2023-04-15 14:42:35 -0500

    intro cutscene is done.

--- source/main.lua
+++ source/main.lua
@@ -53,12 +53,12 @@ local function setup()
 
     -- long in the future, this will be set to the 'title screen scene'
     --local firstScene = DeathScreen()
-    local firstScene = Test1Scene() 
+    --local firstScene = Test1Scene() 
     --local firstScene = CutsceneDemo()
     --local firstScene = DemoScene()
-    local firstScene = MainMenu() -- Uncomment to start at main menu
+    --local firstScene = MainMenu() -- Uncomment to start at main menu
     --local firstScene = Scene0020() -- Uncomment to start from the opening credits
-    --local firstScene = Scene0030() -- Start from first gameplay section
+    local firstScene = Scene0030() -- Start from first gameplay section
     sceneMgr:switchScene(firstScene, SCENE_TRANSITION.FADE_IO)
 end
 
