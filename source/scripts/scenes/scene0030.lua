839894ff3241b28d753c0cac5b567d7e2f8901ce
Jordan Bleu <jordanbleu1@gmail.com>
2023-05-11 08:08:34 -0500

    uncomment scene 30

--- source/scripts/scenes/scene0030.lua
+++ source/scripts/scenes/scene0030.lua
@@ -47,156 +47,156 @@ function Scene0030:initialize(sceneManager)
 
     local segments = {}
 
-    -- table.insert(segments, function()
-    --     return WaitSegment(3000)
-    -- end)
+    table.insert(segments, function()
+        return WaitSegment(3000)
+    end)
 
-    -- table.insert(segments, function()
-    --     -- back in the cokpit again
-    --     return DialogueSegment("scene0030/cyberMonologue.txt", player)
-    -- end)
+    table.insert(segments, function()
+        -- back in the cokpit again
+        return DialogueSegment("scene0030/cyberMonologue.txt", player)
+    end)
 
-    -- table.insert(segments, function()
-    --     Tutorial(gfx.getString("tutorial.move"))
-    --     return WaitSegment(8000)
-    -- end)
+    table.insert(segments, function()
+        Tutorial(gfx.getString("tutorial.move"))
+        return WaitSegment(8000)
+    end)
 
-    -- table.insert(segments, function()
-    --     -- what the hell are these things?
-    --     return DialogueSegment("scene0030/cyberMonologue1.txt", player)
-    -- end)
+    table.insert(segments, function()
+        -- what the hell are these things?
+        return DialogueSegment("scene0030/cyberMonologue1.txt", player)
+    end)
     
-    -- table.insert(segments, function()
-    --     Tutorial(gfx.getString("tutorial.shoot"))
-    --     local enemies = {}
-    --     table.insert(enemies, Grunt(200,-10, camera, player))
-    --     table.insert(enemies, Grunt(100,-20, camera, player))
-    --     return HordeSegment(enemies)
-    -- end)
+    table.insert(segments, function()
+        Tutorial(gfx.getString("tutorial.shoot"))
+        local enemies = {}
+        table.insert(enemies, Grunt(200,-10, camera, player))
+        table.insert(enemies, Grunt(100,-20, camera, player))
+        return HordeSegment(enemies)
+    end)
 
-    -- table.insert(segments, function()
-    --     return WaitSegment(3000)
-    -- end)
+    table.insert(segments, function()
+        return WaitSegment(3000)
+    end)
 
-    -- table.insert(segments, function()
-    --     Tutorial(gfx.getString("tutorial.shoot"))
-    --     local enemies = {}
-    --     table.insert(enemies, Grunt(100,-10, camera, player))
-    --     table.insert(enemies, Grunt(150,-17, camera, player))
-    --     table.insert(enemies, Grunt(200,-20, camera, player))
-    --     table.insert(enemies, Grunt(350,-25, camera, player))
-    --     return HordeSegment(enemies)
-    -- end)
+    table.insert(segments, function()
+        Tutorial(gfx.getString("tutorial.shoot"))
+        local enemies = {}
+        table.insert(enemies, Grunt(100,-10, camera, player))
+        table.insert(enemies, Grunt(150,-17, camera, player))
+        table.insert(enemies, Grunt(200,-20, camera, player))
+        table.insert(enemies, Grunt(350,-25, camera, player))
+        return HordeSegment(enemies)
+    end)
 
 
-    -- table.insert(segments, function()
-    --     return WaitSegment(2000)
-    -- end)
+    table.insert(segments, function()
+        return WaitSegment(2000)
+    end)
 
-    -- table.insert(segments, function()
-    --     Tutorial(gfx.getString("tutorial.dash"))
-    --     local enemies = {}
-    --     table.insert(enemies, Grunt(300,-50, camera, player))
-    --     table.insert(enemies, Grunt(120,-10, camera, player))
-    --     table.insert(enemies, DiveBomb(100, -20, camera, player))
-    --     table.insert(enemies, DiveBomb(30, -30, camera, player))
-    --     table.insert(enemies, DiveBomb(player.x, -30, camera, player))
-    --     return HordeSegment(enemies)
-    -- end)
+    table.insert(segments, function()
+        Tutorial(gfx.getString("tutorial.dash"))
+        local enemies = {}
+        table.insert(enemies, Grunt(300,-50, camera, player))
+        table.insert(enemies, Grunt(120,-10, camera, player))
+        table.insert(enemies, DiveBomb(100, -20, camera, player))
+        table.insert(enemies, DiveBomb(30, -30, camera, player))
+        table.insert(enemies, DiveBomb(player.x, -30, camera, player))
+        return HordeSegment(enemies)
+    end)
 
-    -- table.insert(segments, function()
-    --     return WaitSegment(2000)
-    -- end)
+    table.insert(segments, function()
+        return WaitSegment(2000)
+    end)
     
-    -- table.insert(segments, function()
-    --     local enemies = {}
+    table.insert(segments, function()
+        local enemies = {}
 
-    --     local spd = 0.5
-    --     local dist = 6
+        local spd = 0.5
+        local dist = 6
         
-    --     enemies[1] = TinyShip(100, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[2] = TinyShip(120, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[3] = TinyShip(140, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[4] = TinyShip(160, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[5] = TinyShip(180, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[6] = TinyShip(200, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[7] = TinyShip(220, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[8] = TinyShip(240, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[9] = TinyShip(100, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
-    --     enemies[10] = TinyShip(120, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
-    --     enemies[11] = TinyShip(140, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
-    --     enemies[12] = TinyShip(160, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
-    --     enemies[13] = TinyShip(180, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
-    --     enemies[14] = TinyShip(200, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
-    --     enemies[15] = TinyShip(220, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
-    --     enemies[16] = TinyShip(240, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
+        enemies[1] = TinyShip(100, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[2] = TinyShip(120, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[3] = TinyShip(140, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[4] = TinyShip(160, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[5] = TinyShip(180, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[6] = TinyShip(200, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[7] = TinyShip(220, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[8] = TinyShip(240, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[9] = TinyShip(100, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
+        enemies[10] = TinyShip(120, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
+        enemies[11] = TinyShip(140, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
+        enemies[12] = TinyShip(160, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
+        enemies[13] = TinyShip(180, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
+        enemies[14] = TinyShip(200, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
+        enemies[15] = TinyShip(220, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
+        enemies[16] = TinyShip(240, -40, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(-spd)
         
-    --     return HordeSegment(enemies)
-    -- end)
+        return HordeSegment(enemies)
+    end)
 
-    -- table.insert(segments, function()
-    --     local enemies = {}
+    table.insert(segments, function()
+        local enemies = {}
         
-    --     local spd = 0.5
-    --     local dist = 24
+        local spd = 0.5
+        local dist = 24
 
-    --     enemies[1] = Saucer(50, -50, camera, player)
-    --     enemies[2] = TinyShip(120, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[3] = TinyShip(140, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[4] = TinyShip(160, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[5] = TinyShip(180, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[6] = TinyShip(200, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
-    --     enemies[7] = TinyShip(220, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[1] = Saucer(50, -50, camera, player)
+        enemies[2] = TinyShip(120, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[3] = TinyShip(140, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[4] = TinyShip(160, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[5] = TinyShip(180, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[6] = TinyShip(200, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
+        enemies[7] = TinyShip(220, -20, camera, player):withHorizontalDistance(dist):withHorizontalSpeed(spd)
  
-    --     return HordeSegment(enemies)
-    -- end)
+        return HordeSegment(enemies)
+    end)
 
-    -- table.insert(segments, function()
-    --     return WaitSegment(1000)
-    -- end)
+    table.insert(segments, function()
+        return WaitSegment(1000)
+    end)
 
 
-    -- table.insert(segments, function()
-    --     local enemies = {}
+    table.insert(segments, function()
+        local enemies = {}
         
-    --     enemies[1] = TinyGuy(200,-10,nil, camera, player)
-    --     enemies[2] = TinyGuy(200,-10,enemies[1], camera, player)
-    --     enemies[3] = TinyGuy(200,-20,enemies[2], camera, player)
-    --     enemies[4] = TinyGuy(200,-30,enemies[3], camera, player)
-    --     enemies[5] = TinyGuy(200,-40,enemies[4], camera, player)
-    --     enemies[6] = TinyGuy(200,-50,enemies[5], camera, player)
-    --     enemies[7] = TinyGuy(200,-60,enemies[6], camera, player)
-    --     enemies[8] = TinyGuy(200,-70,enemies[7], camera, player)
+        enemies[1] = TinyGuy(200,-10,nil, camera, player)
+        enemies[2] = TinyGuy(200,-10,enemies[1], camera, player)
+        enemies[3] = TinyGuy(200,-20,enemies[2], camera, player)
+        enemies[4] = TinyGuy(200,-30,enemies[3], camera, player)
+        enemies[5] = TinyGuy(200,-40,enemies[4], camera, player)
+        enemies[6] = TinyGuy(200,-50,enemies[5], camera, player)
+        enemies[7] = TinyGuy(200,-60,enemies[6], camera, player)
+        enemies[8] = TinyGuy(200,-70,enemies[7], camera, player)
         
-    --     return HordeSegment(enemies)
-    -- end)
+        return HordeSegment(enemies)
+    end)
 
-    -- table.insert(segments, function()
-    --     local enemies = {}
+    table.insert(segments, function()
+        local enemies = {}
         
-    --     enemies[1] = TinyGuy(200,-10,nil, camera, player)
-    --     enemies[2] = TinyGuy(200,-10,enemies[1], camera, player)
-    --     enemies[3] = TinyGuy(200,-20,enemies[2], camera, player)
+        enemies[1] = TinyGuy(200,-10,nil, camera, player)
+        enemies[2] = TinyGuy(200,-10,enemies[1], camera, player)
+        enemies[3] = TinyGuy(200,-20,enemies[2], camera, player)
 
-    --     enemies[4] = TinyGuy(200,-30,nil, camera, player)
-    --     enemies[5] = TinyGuy(200,-40,enemies[4], camera, player)
-    --     enemies[6] = TinyGuy(200,-50,enemies[5], camera, player)
+        enemies[4] = TinyGuy(200,-30,nil, camera, player)
+        enemies[5] = TinyGuy(200,-40,enemies[4], camera, player)
+        enemies[6] = TinyGuy(200,-50,enemies[5], camera, player)
 
-    --     enemies[7] = RangedGrunt(200,-25, camera, player):withCooldownRangeBetween(25,100)
+        enemies[7] = RangedGrunt(200,-25, camera, player):withCooldownRangeBetween(25,100)
 
-    --     return HordeSegment(enemies)
-    -- end)
+        return HordeSegment(enemies)
+    end)
 
-    -- table.insert(segments, function()
-    --     return WaitSegment(4000)
-    -- end)
+    table.insert(segments, function()
+        return WaitSegment(4000)
+    end)
 
-    -- table.insert(segments, function()
-    --     local enemies = {}        
-    --     enemies[1] = Grunt(20,-10, camera, player)
-    --     enemies[2] = Grunt(380,-30, camera, player)
-    --     return HordeSegment(enemies)
-    -- end)
+    table.insert(segments, function()
+        local enemies = {}        
+        enemies[1] = Grunt(20,-10, camera, player)
+        enemies[2] = Grunt(380,-30, camera, player)
+        return HordeSegment(enemies)
+    end)
 
     table.insert(segments, function()
         local enemies = {}
